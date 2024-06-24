f517a184474e0bf30e642a9241fda8a38a613ea9
Qedric <qedricjames@gmail.com>
2024-06-24 17:55:00 +0200

    fixed extra space in share link

--- app/frames/trade/txStatusTrade/route.tsx
+++ app/frames/trade/txStatusTrade/route.tsx
@@ -75,7 +75,7 @@ const handleRequest = frames(async (ctx) => {
             }
 
             const addThe = (bulidingName:string) => bulidingName.toLowerCase().startsWith('the') ? bulidingName : `the ${bulidingName}`
-            const removeThe = (bulidingName:string) => bulidingName.toLowerCase().startsWith('the') ? bulidingName.substring(3) : bulidingName
+            const removeThe = (bulidingName:string) => bulidingName.toLowerCase().startsWith('the') ? bulidingName.substring(4) : bulidingName
             console.log('remove string:', removeThe(building.metadata.name))
             console.log('add string:', addThe(building.metadata.name))
             const successString = `${isSell ? "You've parted with" : "You've acquired"} ${ amount > BigInt(1) ? `${amount} ${removeThe(building.metadata.name)} cards!` : `${addThe(building.metadata.name)} card!`}`
